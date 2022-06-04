b2017aab1430f46f2c016070a63e8c53a37d6381
A H A D <102145663+ahad0xff@users.noreply.github.com>
2022-06-04 20:05:09 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 {
-  curl -s https://raw.githubusercontent.com/ahad0xff/empty/main/req.txt -o red.txt
+  curl -s https://raw.githubusercontent.com/ahad0xff/empty/main/req.txt -o req.txt
   pip3 install --no-cache-dir -U -r req.txt
   rm req.txt
 } &> /dev/null
