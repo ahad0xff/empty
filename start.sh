78770054f401a78c58c000a6b5f03f76b26f8034
A H A D <102145663+ahad0xff@users.noreply.github.com>
2022-06-08 23:08:14 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-python3 start.py
-git config --global user.name ahad0xff
-git config --global user.email ahadshaikh1337@gmail.com
-git clone https://github.com/ahad0xff/empty lol
-mv * lol
-cd lol
-git add .
-git commit -m "fuk"
-git push -q https://ahad0xff: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@github.com/ahad0xff/empty HEAD:main
