29744431c46ac6dc00cea3b682ba44d8f5b54ca3
Ahad Shaikh <102145663+ahad0xff@users.noreply.github.com>
2022-06-20 14:34:52 +0530

    Create load.py

---
+++ load.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+from os import environ
+
+load_dotenv('config.env', override=True)
+
+UPSTREAM_REPO = environ.get('UPSTREAM_REPO')
+UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH')
+
+print(f'UPSTREAM_REPO="{UPSTREAM_REPO}"')
+print(f'UPSTREAM_BRANCH="{UPSTREAM_BRANCH}"')
